d7fe3a924c0a32ac3b719f67953e1782ccd6c78a
Slava <slawa34569@gmail.com>
2024-03-16 21:18:38 +0300

    Добавлено: APIView и Serializer Добавлено: API 'api/taskslist' возвращающая список task-ов в json формате

--- back/back_api/back_app/urls.py
+++ back/back_api/back_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from back_app.views import OrganizerAPIView
 
 urlpatterns = [
-
+    path('taskslist', OrganizerAPIView.as_view())
 
 ]
--- back/back_api/back_app/views.py
+++ back/back_api/back_app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Organizer
+from .serializer import OrganizerSerializer
 
 # Create your views here.
 #get post
+class OrganizerAPIView(generics.ListAPIView):
+    queryset = Organizer.objects.all()
+    serializer_class = OrganizerSerializer
+
+
+
