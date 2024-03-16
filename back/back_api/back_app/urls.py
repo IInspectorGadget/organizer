5292e9d6a314c4136faaeaac11940fe8f23def54
Slava <slawa34569@gmail.com>
2024-03-17 00:42:28 +0300

    Добавлено: "api/taskslist/" возвращает список задач Добавлено: "api/createtask/" создаёт запись задачи в БД Добавлено: "api/updatetask/<task_id>/" обновляет запись задачи в БД

--- back/back_api/back_app/urls.py
+++ back/back_api/back_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from back_app.views import OrganizerAPIView
+from back_app.views import *
 
 urlpatterns = [
-    path('taskslist', OrganizerAPIView.as_view())
+    path('taskslist/', GetAllTasksView.as_view()),
+    path('createtask/', CreateNewTaskView.as_view()),
+    path('updatetask/<int:pk>/', UpdateTaskView.as_view())
+
 
 ]
